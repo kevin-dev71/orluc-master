a63a093f11c436a1f340c66644981df98ec4df95
“KevinDev” <kevin.dev71@gmail.com>
2019-03-24 00:17:51 -0300

    now

--- src/server/config.js
+++ src/server/config.js
@@ -18,7 +18,7 @@ module.exports = app => {
     require('../config/passport');
     
     // Settings
-    app.set('port' , process.env.PORT || 3000);
+    app.set('port' , process.env.PORT || 8080);
     app.set('views' , path.join(__dirname, '../views'));
     app.engine('.hbs' , exphbs({
         defaultLayout: 'main',
